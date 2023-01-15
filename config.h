3c97b566390f1a9d006c0cb0c4f145036a625507
gxt_kt <gxt_kt@163.com>
2023-01-16 04:41:11 +0800

    remove rules

--- config.h
+++ config.h
@@ -66,22 +66,22 @@ static char lockfile[] = "/tmp/dwm.lock"; // doublepressquitPatch
 
 static const Rule rules[] = {
     /* class                 instance              title             tags mask     isfloating  isglobal    isnoborder monitor */
-    //{"obs",                  NULL,                 NULL,             1 << 5,       0,          0,          0,        -1 },
+    {"demotest",                  NULL,                 NULL,             1 << 5,       0,          0,          0,        -1 },
     //{"chrome",               NULL,                 NULL,             1 << 6,       0,          0,          0,        -1 },
     //{"Chromium",             NULL,                 NULL,             1 << 6,       0,          0,          0,        -1 },
-    {"music",                NULL,                 NULL,             1 << 7,       1,          0,          1,        -1 },
-    { NULL,                 "qq",                  NULL,             1 << 8,       0,          0,          1,        -1 },
-    { NULL,                 "wechat.exe",          NULL,             1 << 9,       0,          0,          0,        -1 },
-    { NULL,                 "wxwork.exe",          NULL,             1 << 10,      0,          0,          0,        -1 },
-    { NULL,                  NULL,                "broken",          0,            1,          0,          0,        -1 },
-    { "图片查看",           "图片查看",           "图片查看",        0,            1,          0,          0,        -1 },
-    { "图片预览",           "图片预览",           "图片预览",        0,            1,          0,          0,        -1 },
-    { NULL,                  NULL,                "crx_",            0,            1,          0,          0,        -1 },
-    {"flameshot",            NULL,                 NULL,             0,            1,          0,          0,        -1 },
-    {"wemeetapp",            NULL,                 NULL,             TAGMASK,      1,          1,          0,        -1 }, // 腾讯会议在切换tag时有诡异bug导致退出 变成global来规避该问题
-    {"float",                NULL,                 NULL,             0,            1,          0,          0,        -1 }, // 特殊class client默认浮动
-    {"noborder",             NULL,                 NULL,             0,            1,          0,          1,        -1 }, // 特殊class client默认无边框
-    {"global",               NULL,                 NULL,             TAGMASK,      1,          1,          0,        -1 }, // 特殊class client全局于所有tag
+  //  {"music",                NULL,                 NULL,             1 << 7,       1,          0,          1,        -1 },
+  //  { NULL,                 "qq",                  NULL,             1 << 8,       0,          0,          1,        -1 },
+  //  { NULL,                 "wechat.exe",          NULL,             1 << 9,       0,          0,          0,        -1 },
+  //  { NULL,                 "wxwork.exe",          NULL,             1 << 10,      0,          0,          0,        -1 },
+  //  { NULL,                  NULL,                "broken",          0,            1,          0,          0,        -1 },
+  //  { "图片查看",           "图片查看",           "图片查看",        0,            1,          0,          0,        -1 },
+  //  { "图片预览",           "图片预览",           "图片预览",        0,            1,          0,          0,        -1 },
+  //  { NULL,                  NULL,                "crx_",            0,            1,          0,          0,        -1 },
+  //  {"flameshot",            NULL,                 NULL,             0,            1,          0,          0,        -1 },
+  //  {"wemeetapp",            NULL,                 NULL,             TAGMASK,      1,          1,          0,        -1 }, // 腾讯会议在切换tag时有诡异bug导致退出 变成global来规避该问题
+  //  {"float",                NULL,                 NULL,             0,            1,          0,          0,        -1 }, // 特殊class client默认浮动
+  //  {"noborder",             NULL,                 NULL,             0,            1,          0,          1,        -1 }, // 特殊class client默认无边框
+  //  {"global",               NULL,                 NULL,             TAGMASK,      1,          1,          0,        -1 }, // 特殊class client全局于所有tag
 };
 static const char *overviewtag = "OVERVIEW";
 static const Layout overviewlayout = { "舘",  overview };
