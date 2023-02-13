f855ad79b6ef79295fa3d9b0ebccdbb168bba276
gxt_kt <gxt_kt.com>
2023-02-13 21:09:32 +0800

    solve music_title bug

--- statusbar/packages/music_title.sh
+++ statusbar/packages/music_title.sh
@@ -11,10 +11,10 @@ update() {
     # title=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.yesplaymusic /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2 | cut -c -30) 2>/dev/null
     title=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.yesplaymusic /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2) 2>/dev/null
     icon="🎵"
-    text="$title"
+    text="${title}"
 
     sed -i '/^export '$this'=.*$/d' $DWM/statusbar/temp
-    printf "export %s='%s%s%s%s%s'\n" $this "$signal" "$icon_color" "$icon" "$text_color" "$text" >> $DWM/statusbar/temp
+    printf 'export %s="%s%s%s%s%s"\n' $this "$signal" "$icon_color" "$icon" "$text_color" "$text" >> $DWM/statusbar/temp
 }
 
 click() {
