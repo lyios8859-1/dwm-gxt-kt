0c24d284c2124aa9a09116811bf3c71624e370ec
gxt_kt <gxt_kt.com>
2023-02-16 13:40:09 +0800

    update config.def.h

--- config.h
+++ config.h
@@ -140,15 +140,16 @@ static const Layout overviewlayout = { "",  overview };
 static const Layout layouts[] = {
 	/* symbol     arrange function, { nmaster, nstack, layout, master axis, stack axis, secondary stack axis } */
 	{ "﬿",      flextile,         { -1, -1, SPLIT_VERTICAL, TOP_TO_BOTTOM, TOP_TO_BOTTOM, 0, NULL } }, // default tile layout
+  { "﩯", magicgrid,{0} },    /* 网格 */
 	// { "><>",      NULL,             {0} },    /* no layout function means floating behavior */ // 被win+shift+f 替代，不用这个
-	{ "[M]",      flextile,         { -1, -1, NO_SPLIT, MONOCLE, MONOCLE, 0, NULL } }, // monocle
-	{ "|||",      flextile,         { -1, -1, SPLIT_VERTICAL, LEFT_TO_RIGHT, TOP_TO_BOTTOM, 0, NULL } }, // columns (col) layout //其实就是tile
+	// { "[M]",      flextile,         { -1, -1, NO_SPLIT, MONOCLE, MONOCLE, 0, NULL } }, // monocle
+	// { "|||",      flextile,         { -1, -1, SPLIT_VERTICAL, LEFT_TO_RIGHT, TOP_TO_BOTTOM, 0, NULL } }, // columns (col) layout //其实就是tile
 	// { ">M>",      flextile,         { -1, -1, FLOATING_MASTER, LEFT_TO_RIGHT, LEFT_TO_RIGHT, 0, NULL } }, // floating master
-	{ "[D]",      flextile,         { -1, -1, SPLIT_VERTICAL, TOP_TO_BOTTOM, MONOCLE, 0, NULL } }, // deck
+	// { "[D]",      flextile,         { -1, -1, SPLIT_VERTICAL, TOP_TO_BOTTOM, MONOCLE, 0, NULL } }, // deck
 	{ "TTT",      flextile,         { -1, -1, SPLIT_HORIZONTAL, LEFT_TO_RIGHT, LEFT_TO_RIGHT, 0, NULL } }, // bstack
 	{ "===",      flextile,         { -1, -1, SPLIT_HORIZONTAL, LEFT_TO_RIGHT, TOP_TO_BOTTOM, 0, NULL } }, // bstackhoriz
-	{ "|M|",      flextile,         { -1, -1, SPLIT_HORIZONTAL, LEFT_TO_RIGHT, TOP_TO_BOTTOM, 0, monoclesymbols } }, // centeredmaster
-	{ ":::",      flextile,         { -1, -1, NO_SPLIT, GAPPLESSGRID, GAPPLESSGRID, 0, NULL } }, // gappless grid
+	// { ":::",      flextile,         { -1, -1, SPLIT_HORIZONTAL, LEFT_TO_RIGHT, TOP_TO_BOTTOM, 0, monoclesymbols } }, // centeredmaster
+	{ "gapless",      flextile,         { -1, -1, NO_SPLIT, GAPPLESSGRID, GAPPLESSGRID, 0, NULL } }, // gappless grid
 	{ "[\\]",     flextile,         { -1, -1, NO_SPLIT, DWINDLE, DWINDLE, 0, NULL } }, // fibonacci dwindle
 	{ "(@)",      flextile,         { -1, -1, NO_SPLIT, SPIRAL, SPIRAL, 0, NULL } }, // fibonacci spiral
 	{ "[T]",      flextile,         { -1, -1, SPLIT_VERTICAL, LEFT_TO_RIGHT, TATAMI, 0, NULL } }, // tatami mats
@@ -227,9 +228,11 @@ static Key keys[] = {
   	// { MODKEY|ControlMask|ShiftMask, XK_e,      rotatelayoutaxis, {.i = -2 } },  /* flextile, 2 = master axis */
   	// { MODKEY|ControlMask|ShiftMask, XK_r,      rotatelayoutaxis, {.i = -3 } },  /* flextile, 3 = stack axis */
   	// { MODKEY|ControlMask|ShiftMask, XK_t,      rotatelayoutaxis, {.i = -4 } },  /* flextile, 4 = secondary stack axis */
-  	{ MODKEY|ControlMask,           XK_Return, rotatelayoutaxis,   {0} },           /* flextile, flip master and stack areas */
-	  { MODKEY|ShiftMask,      XK_comma, rotatelayoutaxis,    {.i = -1 } },
-	  { MODKEY|ShiftMask,      XK_period, rotatelayoutaxis,    {.i = +1 } },
+  	{ MODKEY|ControlMask,           XK_Return, mirrorlayout,   {0} },           /* flextile, flip master and stack areas */
+	  { MODKEY|ShiftMask,      XK_comma, cyclelayout,    {.i = -1 } },
+	  { MODKEY|ShiftMask,      XK_period, cyclelayout,    {.i = +1 } },
+	  { MODKEY|ControlMask,      XK_comma, rotatelayoutaxis,    {.i = -1 } },
+	  { MODKEY|ControlMask,      XK_period, rotatelayoutaxis,    {.i = +1 } },
   // flextile
 
     { MODKEY|Mod1Mask,              XK_Left,             focusmon,         {.i = -1} },               /* super b            |  光标移动到另一个显示器 */
